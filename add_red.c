9948278fd900827f824fb8cbc21cdb17e14f4b23
Daniel Kreling <dkreling@daitangroup.com>
2018-01-17 16:12:10 -0200

    red: add lorem_ipsum to the file
    Insert some sample contents to test conflicts
    
    Signed-off-by: Daniel Kreling <dkreling@daitangroup.com>

--- add_red.c
+++ add_red.c
@@ -1,6 +1,10 @@
 This file adds a new color:
 
-Red is the color.
+Red is the color of the screen.
+
+Adding lorem ipsum:
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut massa quis nibh venenatis pellentesque. Quisque euismod purus sit amet rhoncus iaculis. Mauris suscipit sapien iaculis nisi vulputate suscipit. Nullam malesuada, odio non facilisis sagittis, ex turpis condimentum nunc, feugiat interdum est lorem lobortis ex. Praesent in risus ut leo porttitor mollis. Cras ac mollis diam. Maecenas sapien lorem, placerat sit amet porta ut, auctor eget augue. Nam molestie accumsan eros, nec bibendum risus dignissim at. Fusce sed dapibus arcu. 
 
 
 It's color is red.
